7027dbf4147ff5c9b3ba13be994c2d0441d06db0
CommanderBeelo <xxxbilliamxxx@gmail.com>
2023-06-14 12:10:13 -0400

    Main Menu Music
    This still worked in the old template, so I am bringing it over to the new one if you allow it O' Divine One.

--- LoadingScreen/Scripts/config.cpp
+++ LoadingScreen/Scripts/config.cpp
@@ -38,3 +38,21 @@ class CfgMods
 		};
 	};
 };
+class CfgSoundSets
+{
+	class Main_Music_Menu_SoundSet
+	{
+		soundShaders[] = {"Main_Music_Menu_SoundShader"};
+		volumeFactor = 1;
+		frequencyFactor = 1;
+		spatial = 0;
+	};
+};	
+class CfgSoundShaders
+{
+	class Main_Music_Menu_SoundShader
+	{
+		samples[] = {{"LoadingScreen\Music\mainmenu.ogg",1}};
+		volume = 1.00000000;
+	};
+};
